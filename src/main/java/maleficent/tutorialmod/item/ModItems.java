ff1538804f51182ba2f00aa93b703d258657394c
Adrian <adrian.mihu20@icloud.com>
2025-04-21 23:53:05 +0300

    Added pink weapon item

---
+++ src/main/java/maleficent/tutorialmod/item/ModItems.java
@@ -0,0 +1,31 @@
+package maleficent.tutorialmod.item;
+
+import maleficent.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_WEAPON = registerItem("pink_weapon",
+            new AxeItem(ToolMaterial.GOLD,100,-2.4f, new Item.Settings()
+                    .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, "pink_weapon")))));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries ->{
+            entries.add(PINK_WEAPON);
+        });
+    }
+}
